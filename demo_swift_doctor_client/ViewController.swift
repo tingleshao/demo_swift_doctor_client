a2f08ee2245ab46f36669bc42b869712d40f4fef
tingleshao <cshao@cs.unc.edu>
2017-03-18 14:10:00 -0400

    Initial Commit

---
+++ demo_swift_doctor_client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  demo_swift_doctor_client
+//
+//  Created by CHONG SHAO on 3/18/17.
+//  Copyright © 2017 CHONG SHAO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
